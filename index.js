979974f619762c7c7a0c943e1a282c1aca7f7de3
vsvirskiy <vsvirskiy98@gmail.com>
2025-06-16 17:13:05 +0300

    add statistic middleware

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const remeberHost = require('./middlewares/statistics');
 
 const PORT = 5000;
 
 const userRouter = require('./routers/user-router');
+
+// statistics
+app.use(remeberHost);
 
 app.use(express.json());
 app.use('/api', userRouter);
@@ -24,7 +28,9 @@ app.use((err, req, res, next) => {
     res.status(500).send("Error, server says: " + `"${errDetailMsg}"`);
 
     next();
-})
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
